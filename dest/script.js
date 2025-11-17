5bfe5ed241c26f7fb9c5afa9bf3c4deb7b67996b
Dylan Lamb <lambd1@wit.edu>
2025-11-17 15:41:28 -0500

    added button to the  websites github page

--- dest/script.js
+++ dest/script.js
@@ -3,11 +3,15 @@
 const FLAPPYBIRDLINKID = "FlappyBirdGithubButton";
 const FLAPPYBIRDURL = "https://github.com/DLamb-TheFallen/Flappy-Bird-Python";
 // Constants for the game of life github button
-const GAMEOFLIFELINKID = "GameOfLifeGitHubButton";
+const GAMEOFLIFELINKID = "GameOfLifeGithubButton";
 const GAMEOFLIFEURL = "https://github.com/DLamb-TheFallen/Conway-s-Game-of-Life-Java-";
+// Constants for website github button
+const WEBSITELINKID = "WebsiteGithubButton";
+const WEBSITEURL = "https://github.com/DLamb-TheFallen/DLamb-TheFallen.github.io";
 // Add click functions to buttons
 linkButton(FLAPPYBIRDLINKID, FLAPPYBIRDURL);
 linkButton(GAMEOFLIFELINKID, GAMEOFLIFEURL);
+linkButton(WEBSITELINKID, WEBSITEURL);
 function linkButton(buttonId, link) {
     document.getElementById(buttonId)?.addEventListener("click", function () {
         window.open(link, "_blank");
--- src/script.ts
+++ src/script.ts
@@ -4,14 +4,18 @@ const FLAPPYBIRDLINKID: string = "FlappyBirdGithubButton";
 const FLAPPYBIRDURL: string = "https://github.com/DLamb-TheFallen/Flappy-Bird-Python"
 
 // Constants for the game of life github button
-const GAMEOFLIFELINKID: string = "GameOfLifeGitHubButton";
+const GAMEOFLIFELINKID: string = "GameOfLifeGithubButton";
 const GAMEOFLIFEURL: string = "https://github.com/DLamb-TheFallen/Conway-s-Game-of-Life-Java-";
+
+// Constants for website github button
+const WEBSITELINKID: string = "WebsiteGithubButton";
+const WEBSITEURL: string = "https://github.com/DLamb-TheFallen/DLamb-TheFallen.github.io";
 
 
 // Add click functions to buttons
 linkButton(FLAPPYBIRDLINKID, FLAPPYBIRDURL);
 linkButton(GAMEOFLIFELINKID, GAMEOFLIFEURL);
-
+linkButton(WEBSITELINKID, WEBSITEURL);
 
 
 
