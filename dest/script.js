9c214ee2ad7a00fade7c19e8010811e82842f51d
Dylan Lamb <lambd1@wit.edu>
2025-11-16 20:13:02 -0500

    Added game of life into projects

--- dest/script.js
+++ dest/script.js
@@ -1,7 +1,13 @@
 "use strict";
+// Constants for the flappy bird github button
 const FLAPPYBIRDLINKID = "FlappyBirdGithubButton";
 const FLAPPYBIRDURL = "https://github.com/DLamb-TheFallen/Flappy-Bird-Python";
+// Constants for the game of life github button
+const GAMEOFLIFELINKID = "GameOfLifeGitHubButton";
+const GAMEOFLIFEURL = "https://github.com/DLamb-TheFallen/Conway-s-Game-of-Life-Java-";
+// Add click functions to buttons
 linkButton(FLAPPYBIRDLINKID, FLAPPYBIRDURL);
+linkButton(GAMEOFLIFELINKID, GAMEOFLIFEURL);
 function linkButton(buttonId, link) {
     document.getElementById(buttonId)?.addEventListener("click", function () {
         window.open(link, "_blank");
--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,16 @@
 
+// Constants for the flappy bird github button
 const FLAPPYBIRDLINKID: string = "FlappyBirdGithubButton";
 const FLAPPYBIRDURL: string = "https://github.com/DLamb-TheFallen/Flappy-Bird-Python"
 
-linkButton(FLAPPYBIRDLINKID, FLAPPYBIRDURL)
+// Constants for the game of life github button
+const GAMEOFLIFELINKID: string = "GameOfLifeGitHubButton";
+const GAMEOFLIFEURL: string = "https://github.com/DLamb-TheFallen/Conway-s-Game-of-Life-Java-";
 
+
+// Add click functions to buttons
+linkButton(FLAPPYBIRDLINKID, FLAPPYBIRDURL);
+linkButton(GAMEOFLIFELINKID, GAMEOFLIFEURL);
 
 
 
